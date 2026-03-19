497275bf5ce8a685374105bbd42610c4d0b6f2c7
Aubscure <aubreyamante19@gmail.com>
2026-03-19 22:58:13 +0800

    first commit

---
+++ src/sanity/client.ts
@@ -0,0 +1,13 @@
+import { createClient } from "next-sanity";
+
+// We assign these to variables so we can use environment variables later for security
+const projectId = "YOUR_PROJECT_ID"; // We will replace this shortly
+const dataset = "production";
+const apiVersion = "2024-01-01"; // Use today's date or the date you started the project
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, // Set to false if you want to ensure fresh data during development
+});
